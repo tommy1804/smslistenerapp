f2d1c5cf80425b322b58d984715d5dd9103f7a0f
tommy1804 <jangkrik2380@gmailcom>
2020-12-04 10:14:03 +0700

    1817051072

---
+++ app/src/main/java/com/dicoding/associate/smslistenerapp/MainActivity.java
@@ -0,0 +1,15 @@
+package com.dicoding.associate.smslistenerapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.dicoding.mybroadcastreceiver.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
